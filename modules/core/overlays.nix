bc3c7b8e7431dd3b747f2b69c38d8d8f070e64b6
Don Williams <don.e.williams@gmail.com>
2026-02-16 22:00:26 -0500

    Added overlay for tumbler disable EPUB causes webkitgtk to builld source
    Only thumbnails for EPUB files are impacted
    
    /nix/store/fn300dz18wisc4nkbhmhgs2mnxd1yj91-nixos-system-zaneyos-next-25.11.20260208.17b3e84
    └───/nix/store/wb79p57khq59qxy5902dggldh93hjicp-system-path
        └───/nix/store/adaiszkbn3p7x0m2m11dziaam2gp82kn-tumbler-4.20.1
            └───/nix/store/wylxx7axqwzf7miy4g30m3g6j959328k-libgepub-0.7.3
                └───/nix/store/4i6mqkahm
    
     On branch zos-next
     Your branch is up to date with 'origin/zos-next'.
    
     Changes to be committed:
    	modified:   modules/core/overlays.nix

--- modules/core/overlays.nix
+++ modules/core/overlays.nix
@@ -10,5 +10,14 @@
         config.allowUnfree = true;
       };
     })
+
+    # Build tumbler without EPUB thumbnailer (libgepub) to avoid webkitgtk
+    (_final: prev: {
+      xfce = prev.xfce // {
+        tumbler = prev.xfce.tumbler.overrideAttrs (old: {
+          buildInputs = prev.lib.remove prev.libgepub old.buildInputs;
+        });
+      };
+    })
   ];
 }
