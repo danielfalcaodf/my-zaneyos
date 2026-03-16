872fb5556cf79b08dfa162c33e020b963510c269
Don Williams <don.e.williams@gmail.com>
2026-03-16 04:12:44 -0400

    Updated CHANGELOG and version

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -32,7 +32,7 @@ in {
   };
   environment.variables = {
     NIXOS_OZONE_WL = "1";
-    ZANEYOS_VERSION = "2.5.NG";
+    ZANEYOS_VERSION = "2.6.Next";
     ZANEYOS = "true";
   };
   console.keyMap = "${consoleKeyMap}";
