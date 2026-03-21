2bed1729a54db0619681a71a81170340493939c7
Don Williams <don.e.williams@gmail.com>
2026-03-20 22:31:41 -0400

    Update host settings
     On branch zos-next
     Your branch is up to date with 'origin/zos-next'.
    
     Changes to be committed:
    	modified:   hosts/zaneyos-next/variables.nix

--- hosts/zaneyos-next/variables.nix
+++ hosts/zaneyos-next/variables.nix
@@ -87,10 +87,10 @@ _: {
     # This will set your color palette
     # Default background
     # Add new images to ~/zaneyos/wallpapers
-    stylixImage = ../../wallpapers/mountainscapedark.jpg;
+    #stylixImage = ../../wallpapers/mountainscapedark.jpg;
     #stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
     #stylixImage = ../../wallpapers/Anime-Purple-eyes.png;
-    #stylixImage = ../../wallpapers/Rainnight.jpg;
+    stylixImage = ../../wallpapers/Rainnight.jpg;
     #stylixImage = ../../wallpapers/zaney-wallpaper.jpg;
     #stylixImage = ../../wallpapers/nix-wallpapers-strips-logo.jpg;
     #stylixImage = ../../wallpapers/beautifulmountainscape.jpg;
