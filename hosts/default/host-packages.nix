9c0c13d3ca7cd93bcef6a4a66ed84cec532013b1
Don Williams <don.e.williams@gmail.com>
2026-05-09 14:51:24 -0400

    Added cpufrew power mgmt but disabled by default
     On branch zos-next-grub
     Your branch is up to date with 'origin/zos-next-grub'.
    
     Changes to be committed:
    	modified:   hosts/default/host-packages.nix

--- hosts/default/host-packages.nix
+++ hosts/default/host-packages.nix
@@ -1,4 +1,30 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: {
+
   environment.systemPackages = with pkgs; [
   ];
+
+  # Add host specific flatpaks here
+  services = {
+    flatpak = {
+      packages = [
+      ];
+    };
+  };
+
+  # For power mangement on laptops 
+
+  services.auto-cpufreq.enable = false;
+  services.auto-cpufreq.settings = {
+    battery = {
+       governor = "powersave";
+       turbo = "never";
+    };
+    charger = {
+       governor = "performance";
+       turbo = "auto";
+    };
+  };
+
+
+
 }
