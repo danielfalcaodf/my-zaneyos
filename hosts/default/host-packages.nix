1b34c0902a3961321ac8f766aced08c2e34b87d7
Don Williams <don.e.williams@gmail.com>
2026-05-09 14:44:37 -0400

    Added cpu-freq code but left disabled
    {config,pkgs, ...}: {
    
      environment.systemPackages = with pkgs; [
      ];
    
      services.auto-cpufreq.enable = false;
      services.auto-cpufreq.settings = {
        battery = {
           governor = "powersave";
           turbo = "never";
        };
        charger = {
           governor = "performance";
           turbo = "auto";
        };
      };
    
    }
    
     On branch zos-next
     Your branch is up to date with 'origin/zos-next'.
    
     Changes to be committed:
    	modified:   hosts/default/host-packages.nix

--- hosts/default/host-packages.nix
+++ hosts/default/host-packages.nix
@@ -1,4 +1,18 @@
-{pkgs, ...}: {
+{config,pkgs, ...}: {
+
   environment.systemPackages = with pkgs; [
   ];
+
+  services.auto-cpufreq.enable = false;
+  services.auto-cpufreq.settings = {
+    battery = {
+       governor = "powersave";
+       turbo = "never";
+    };
+    charger = {
+       governor = "performance";
+       turbo = "auto";
+    };
+  };
+
 }
