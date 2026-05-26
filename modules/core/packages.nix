99611d406c8983c892a77364ba4122cdd8217401
dwilliams <don.e.williams@gmail.com>
2026-05-26 11:23:02 -0400

    Upd flake added cliamp
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   flake.lock
    	modified:   modules/core/packages.nix

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -52,6 +52,7 @@ in {
       appimage-run # Needed For AppImage Support
       brave # Brave Browser
       brightnessctl # For Screen Brightness Control
+      cliamp # terminal music player
       cliphist # Clipboard manager using rofi menu
       cmatrix # Matrix Movie Effect In Terminal
       cowsay # Great Fun Terminal Program
