4fe597e7c3a2e3d771868416d423fdbf9a19af13
dwilliams <don.e.williams@gmail.com>
2026-08-05 13:55:41 -0400

    Switch to ly
    Signed-off-by: dwilliams <don.e.williams@gmail.com>

--- hosts/zaneyos-24-vm/variables.nix
+++ hosts/zaneyos-24-vm/variables.nix
@@ -7,7 +7,7 @@
   # `tui` for Text login
   # `sddm` for graphical GUI (default)
   # SDDM background is set with stylixImage
-  displayManager = "sddm";
+  displayManager = "tui";
 
   # Emable/disable bundled applications
   tmuxEnable = true;
