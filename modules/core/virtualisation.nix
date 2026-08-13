a519310e57f3dd37476c76a49d069dd0d80a6528
dwilliams <don.e.williams@gmail.com>
2026-08-13 09:17:59 -0400

    fixed systemd-machined build error
    Signed-off-by: dwilliams <don.e.williams@gmail.com>
    
     On branch lua
     Your branch is ahead of 'origin/lua' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   modules/core/virtualisation.nix

--- modules/core/virtualisation.nix
+++ modules/core/virtualisation.nix
@@ -21,6 +21,11 @@
     virt-manager.enable = false;
   };
 
+  systemd.services.systemd-machined = {
+    stopIfChanged = false;
+    restartIfChanged = false;
+  };
+
   environment.systemPackages = with pkgs; [
     virt-viewer # View Virtual Machines
     lazydocker
