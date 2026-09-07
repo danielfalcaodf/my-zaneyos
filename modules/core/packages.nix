17ad3808f59bae1375b22d9942081c6cfbe3fced
dwilliams <don.e.williams@gmail.com>
2026-09-07 14:38:27 -0400

    Changed to brave-origin browser
    Signed-off-by: dwilliams <don.e.williams@gmail.com>
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   modules/core/packages.nix

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -48,7 +48,7 @@ in {
     ++ [
       alejandra # nix formatter
       amfora # Fancy Terminal Browser For Gemini Protocol
-      brave # Brave Browser
+      brave-origin # Brave Browser
       brightnessctl # For Screen Brightness Control
       cliamp # terminal music player
       cliphist # Clipboard manager using rofi menu
