7d7d9703104a0b8c4967951ad39192b821252873
Don Williams <don.e.williams@gmail.com>
2026-09-08 02:03:58 -0400

    Removing uswm from SDDM
    Signed-off-by: Don Williams <don.e.williams@gmail.com>
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   modules/core/sddm.nix

--- modules/core/sddm.nix
+++ modules/core/sddm.nix
@@ -63,6 +63,15 @@
   };
 in {
   services.displayManager = {
+    defaultSession = "hyprland";
+    sessionPackages = lib.mkForce [
+      (pkgs.runCommand "hyprland-session" {
+        passthru.providedSessions = ["hyprland"];
+      } ''
+        mkdir -p $out/share/wayland-sessions
+        cp ${pkgs.hyprland}/share/wayland-sessions/hyprland.desktop $out/share/wayland-sessions/
+      '')
+    ];
     sddm = {
       package = pkgs.kdePackages.sddm;
       extraPackages = [sddm-astronaut];
@@ -74,6 +83,9 @@ in {
         keyboardLayout = vars.keyboardLayout or "us";
         keyboardVariant = vars.keyboardVariant or "";
       in {
+        General = {
+          DefaultSession = "hyprland.desktop";
+        };
         X11 = {
           XkbLayout = keyboardLayout;
           XkbVariant = keyboardVariant;
@@ -81,15 +93,6 @@ in {
       };
     };
   };
-
-  # Mask hyprland-uwsm desktop file so it doesn't appear in SDDM
-  environment.etc."xdg/wayland-sessions/hyprland-uwsm.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Name=Hyprland (uwsm-managed)
-    NoDisplay=true
-    Hidden=true
-  '';
 
   # Ensure Wayland SDDM also sees XKB defaults
   systemd.services.display-manager.environment = let
