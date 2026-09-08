fa3c7e739b9e661f591bacc08bf0692d169b2b84
Don Williams <don.e.williams@gmail.com>
2026-09-08 00:55:16 -0400

    Set SDDM and ghostty to true
    Signed-off-by: Don Williams <don.e.williams@gmail.com>

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -7,13 +7,13 @@
   # `tui` for Text login
   # `sddm` for graphical GUI (default)
   # SDDM background is set with stylixImage
-  displayManager = "tui";
+  displayManager = "sddm";
 
   # Emable/disable bundled applications
   tmuxEnable = false;
   alacrittyEnable = false;
   weztermEnable = false;
-  ghosttyEnable = false;
+  ghosttyEnable = true;
   vscodeEnable = false;
   # Note: This is evil-helix with VIM keybindings by default
   helixEnable = false;
